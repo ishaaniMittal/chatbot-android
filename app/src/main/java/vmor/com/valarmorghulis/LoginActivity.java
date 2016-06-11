c18648070b86e5259155228901639091fd014e39
Vijay Mulani <mulani.vijay19@gmail.com>
2016-06-11 16:01:51 +0530

    First commit

---
+++ app/src/main/java/vmor/com/valarmorghulis/LoginActivity.java
@@ -0,0 +1,15 @@
+package vmor.com.valarmorghulis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vijay on 11/06/2016.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ app/src/main/java/vmor/com/valarmorghulis/MainActivity.java
@@ -0,0 +1,83 @@
+package vmor.com.valarmorghulis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    private ImageView splashImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Splash screen animation boot up
+        splashImage=(ImageView)findViewById(R.id.splashImage);
+        Animation scalein= AnimationUtils.loadAnimation(this,R.anim.scale_in);
+        scalein.setAnimationListener(this);
+        splashImage.setAnimation(scalein);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        TextView splashText=(TextView)findViewById(R.id.splashText);
+        splashText.setVisibility(View.VISIBLE);
+        final Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent=new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(loginIntent);
+                MainActivity.this.finish();
+            }
+        },1000);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
